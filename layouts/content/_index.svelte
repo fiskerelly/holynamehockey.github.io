df369551090ef9592e8bf931a0cbcaa470d35dfd
jimafisk <jim.fisk@jantcu.com>
2024-06-05 21:17:37 -0400

    Initial commit.

---
+++ layouts/content/_index.svelte
@@ -0,0 +1,49 @@
+<script>
+	export let title;
+</script>
+
+<h1>{title}</h1>
+<div class="details">
+	<div class="time">Sunday, September 15, 2024</div>
+	<div class="place">
+		<a href="https://www.eastmountaincc.com/" target="blank" rel="noopener noreferrer">East Mountain Country Club</a>
+	</div>
+	<div class="schedule">
+		<h3>Schedule</h3>
+		<p>11:30</p>
+		<p>1:00</p>
+		<p>Dinner</p>
+	</div>
+	<div class="fees">
+		<p>Player</p>
+		<p>Foursome</p>
+		<p>Guest</p>
+	</div>
+	<div class="venmo">
+		<div class="name">Tim</div>
+		<div class="handle">@Timothy-Harper-23</div>
+		<div class="qr-code">
+			<img src="/venmo.jpg" alt="QR code to pay with venmo" />
+		</div>
+	</div>
+	<div class="checks">
+		<div class="payable">
+			<strong>Payable To:</strong>
+			<div>Timothy Harper</div>
+		</div>
+		<div class="address">
+			<strong>Mail To</strong>
+			<div>24 Atkins St</div>
+			<div>Chicopee, MA 01013</div>
+		</div>
+	</div>
+	<div class="contact">
+		<div>Contact Laura</div>
+	</div>
+</div>
+
+<style>
+	h1 {
+		font-family: Helvetica, sans-serif;
+	}
+</style>
---
+++ layouts/global/head.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let title, env;
+</script>
+
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width,initial-scale=1'>
+
+  <title>{ title }</title>
+  <title>Title of the document</title>
+
+  <script type="module" src="/spa/core/main.js"></script>
+
+  <link rel="icon" type="image/svg+xml" href="/media/logo.svg">
+  <link rel='stylesheet' href="/spa/bundle.css">
+  <link rel='stylesheet' href="/global.css">
+</head>
